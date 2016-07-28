a642991893e9d9285f330af3f5d18a6ffa23fc2e
cerious <your.ceriousn@yahoo.com>
2016-07-28 14:09:57 -0400

    added functionality to the signup page.

--- simple_app/app/controllers/users_controller.rb
+++ simple_app/app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
-  def new
+
+  def show
+    @user = User.find(parama[:id])
+    debugger
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(suer_params)
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      prams.require(:user).permit(:name, :email, :password,
+                                  :password_confirmation)
+    end
 end
