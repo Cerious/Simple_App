3d69b52404b8399412b79e0b09257c1eadedc881
cerious <your.ceriousn@yahoo.com>
2016-08-02 21:21:23 -0400

    changed the layout links.

--- simple_app/app/helpers/sessions_helper.rb
+++ simple_app/app/helpers/sessions_helper.rb
@@ -11,5 +11,9 @@ module SessionsHelper
 
   # Returns true if the user is logged in, false otherwise.
   def logged_in?
-    !current_user.nil?
+    !@current_user.nil?
+  end
+
+
+  # Returns true if the user is logged in, false otherwise.
 end
