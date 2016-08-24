eb3808a923b658131ee6ba0177db2c7e7353a62e
cerious <your.ceriousn@yahoo.com>
2016-08-23 23:22:08 -0400

    writting integration test for route layouts.

--- simple_app/app/controllers/users_controller.rb
+++ simple_app/app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:user, :edit, :update]
   before_action :correct_user, only: [:edit, :udpate]
+
+  def index
+  end
 
   def show
     @user = User.find(params[:id])
--- simple_app/test/integration/users_edit_test.rb
+++ simple_app/test/integration/users_edit_test.rb
@@ -23,6 +23,14 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_template 'user/edit'
     name = "Foo Bar"
     email = "foo@bar.com"
-    patch user_path(@user),
+    patch user_path(@user), params: { user: { name:  name,
+                                              email: email,
+                                              password:              "",
+                                              password_confirmation: "" } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email 
   end
 end
